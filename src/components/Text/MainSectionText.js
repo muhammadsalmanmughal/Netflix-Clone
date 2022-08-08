8d424752a26daaad98e133915e5bd4a39d9f9ae5
muhammadsalmanmughal <meandmylearn@gmail.com>
2022-08-08 18:39:36 +0500

    fix: remove antd grid

--- src/components/Text/MainSectionText.js
+++ src/components/Text/MainSectionText.js
@@ -1,16 +1,13 @@
 import React from "react";
-import { Row, Col } from "antd";
 import "./style.css";
-import '../../App.css'
+import "../../App.css";
 
 const MainSectionText = (props) => {
   return (
-    <Row gutter={[10, 10]}>
-      <Col xs={24} sm={24} md={24} lg={24} className='text-section'>
-        <h1 className="title zero_margin">{props.title}</h1>
-        <h2 className="subtitle zero_margin">{props.subtitle}</h2>
-      </Col>
-    </Row>
+    <div className="our-main-section-text">
+      <h1 className="title zero_margin">{props.title}</h1>
+      <h2 className="subtitle zero_margin">{props.subtitle}</h2>
+    </div>
   );
 };
 export default MainSectionText;
