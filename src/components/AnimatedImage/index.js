674b3d01be0642a5f4b909047bf9488ada8bfd7d
muhammadsalmanmughal <meandmylearn@gmail.com>
2022-08-09 15:07:23 +0500

    fix: remove antd from textandimage and fix responsiveness

--- src/components/AnimatedImage/index.js
+++ src/components/AnimatedImage/index.js
@@ -2,23 +2,24 @@ import React from "react";
 import "./style.css";
 
 const AnimatedImage = (props) => {
-  console.log("AnimatedImageprops: ", props);
   return (
-    <div className="our-main-section-animated-image-container">
-      <img src={props.image} className="main-section-img" />
-      {props.video && (
-        <div className="our-main-section-animation">
-          <video
-            className=""
-            autoplay="true"
-            playsinline="true"
-            muted="true"
-            loop="true"
-          >
-            <source src={props.video} type="video/mp4" />
-          </video>
-        </div>
-      )}
+    <div className="our-main-section-image-container">
+      <div className="our-main-section-animated-image-container">
+        <img src={props.image} className="main-section-img" />
+        {props.video && (
+          <div className="our-main-section-animation">
+            <video
+              className="our-main-section-video"
+              autoplay="true"
+              playsinline="true"
+              muted="true"
+              loop="true"
+            >
+              <source src={props.video} type="video/mp4" />
+            </video>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/containers/MainSection/textAndImage.js
+++ src/containers/MainSection/textAndImage.js
@@ -1,57 +1,16 @@
 import React from "react";
 import { Row, Col } from "antd";
-import { Text } from "../../components";
+import { Text, AnimatedImage } from "../../components";
 
 import "./style.css";
 const TextAndImage = (props) => {
   return (
-    <Row className="mainsection">
-      <Col span={24} className="mainsection-container">
-        <Row className="mainsection-container-text">
-          <Col
-            xs={24}
-            sm={24}
-            md={24}
-            lg={12}
-            // span={7}
-            //   style={{background:'red'}}
-            className="mainsection-text"
-          >
-            <Text.MainSectionText
-              title={props.title}
-              subtitle={props.subtitle}
-            />
-          </Col>
-          <Col
-            xs={24}
-            sm={24}
-            md={24}
-            lg={12}
-            // span={8}
-            //   style={{background:'blue'}}
-          >
-            <Row>
-              <Col span={24}>
-                <img src={props.img} className="img" />
-              </Col>
-              {props.video && (
-                <Col span={24} className="videoBox">
-                  <video
-                    className="video"
-                    autoplay="true"
-                    playsinline="true"
-                    muted="true"
-                    loop="true"
-                  >
-                    <source src={props.video} type="video/mp4" />
-                  </video>
-                </Col>
-              )}
-            </Row>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
+    <div className="our-main-section">
+      <div className="our-main-section-container">
+        <Text.MainSectionText title={props.title} subtitle={props.subtitle} />
+        <AnimatedImage image={props.img} video={props.video} />
+      </div>
+    </div>
   );
 };
 export default TextAndImage;
