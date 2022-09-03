a0bdf622000c978de899031c06b69f349d5f031c
muhammadsalmanmughal <meandmylearn@gmail.com>
2022-09-03 15:04:14 +0500

    feat: Login form

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { Images } from "../../config";
+
+import "./style.css";
 
 const Login = () => {
-    return(
-        <div>
-            <h1>Login</h1>
+  return (
+    <div className="login-wrapper">
+      <div className="login-background">
+        <img className="login-background-img" src={Images.Bg_Login} />
+      </div>
+      <div className="login-header">
+        <a href="">
+          <img className="login-header-logo" src={Images.Logo} />
+        </a>
+      </div>
+      <div className="login-body">
+        <div className="login-body-content">
+          <div className="login-body-content-form">
+            <h1>Sign In</h1>
+            <input placeholder="email" />
+            <input placeholder="password" />
+            <button>Sign In</button>
+          </div>
+          <div className="login-body-content-others"></div>
         </div>
-    )
-}
-export default Login
+      </div>
+      <div className="login-footer"></div>
+
+      <h1>Login</h1>
+    </div>
+  );
+};
+export default Login;
