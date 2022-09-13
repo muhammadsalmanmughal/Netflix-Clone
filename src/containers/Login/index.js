175fbb0840daedb49d69cf382b29caab7a700085
muhammadsalmanmughal <meandmylearn@gmail.com>
2022-09-13 09:27:12 +0500

    feat: login form remember me

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,49 +1,52 @@
 import React from "react";
 import { Images } from "../../config";
 import { Text, InputFields, Buttons } from "../../components";
+import { Checkbox } from "antd";
 
 import "./style.css";
 
 const Login = () => {
   return (
     <div className="login-wrapper">
-    <div className="login-background">
-      <img className="login-background-img" src={Images.Bg_Login} />
-    </div>
-    <div className="login-header">
-      <a href="">
-        <img className="login-header-logo" src={Images.Logo} />
-      </a>
-    </div>
-    <div className="login-body">
-      <div className="login-body-content">
-        <div className="login-body-content-form">
-          <Text.HeadingOne
-            text="Sign In"
-            style={{
-              color: "#fff",
-              fontSize: "32px",
-              fontWeight: 700,
-              marginBottom: "28px",
-            }}
-          />
-          <InputFields.BasicInputField
-            placeholder="Email or phone number"
-            type="email"
-             className='input-field'
-          />
-          <InputFields.BasicInputField
-            placeholder="Password"
-            type="password"
-             className='input-field'
-          />
-          <Buttons.BasicButton text='Sign In' className='login-btnSignIn'/>
-        </div>
-        <div className="login-body-content-others"></div>
+      <div className="login-background">
+        <img className="login-background-img" src={Images.Bg_Login} />
       </div>
+      <div className="login-header">
+        <a href="">
+          <img className="login-header-logo" src={Images.Logo} />
+        </a>
+      </div>
+      <div className="login-body">
+        <div className="login-body-content">
+          <div className="login-body-content-form">
+            <Text.HeadingOne
+              text="Sign In"
+              style={{
+                color: "#fff",
+                fontSize: "32px",
+                fontWeight: 700,
+                marginBottom: "28px",
+              }}
+            />
+            <InputFields.BasicInputField
+              placeholder="Email or phone number"
+              type="email"
+              className="input-field"
+            />
+            <InputFields.BasicInputField
+              placeholder="Password"
+              type="password"
+              className="input-field"
+            />
+            <Buttons.BasicButton text="Sign In" className="login-btnSignIn" />
+          </div>
+          <div className="login-body-content-others">
+            <Checkbox className="login-rememberMe">Remember me</Checkbox>
+          </div>
+        </div>
+      </div>
+      <div className="login-footer"></div>
     </div>
-    <div className="login-footer"></div>
-  </div>
   );
 };
 export default Login;
