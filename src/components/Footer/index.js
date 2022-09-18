3341a70f7a2e5e9d6f25a441c036907e214775a5
muhammadsalmanmughal <meandmylearn@gmail.com>
2022-09-18 10:40:45 +0500

    fix: create footer links a component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,17 @@
 import React from "react";
+import FooterLinks from "./footerLinks";
+
 import "./style.css";
 
-const Footer = props => {
+const Footer = (props) => {
   return (
-    <div className="footer-section">
-      <div className="footer-section-wrapper">
-        <div className="footer">
-          <p className="footer-top">{props.footerTop}</p>
-          <ul className="footer-links">
-            {props.links.map((item) => {
-              return <li className="footer-links-item">{item}</li>;
-            })}
-          </ul>
-          <p className="footer-country">{props.footerCountry}</p>
-        </div>
+    <div className="footer-section-wrapper">
+      <div className="footer">
+        <FooterLinks
+          links={props.links}
+          footerTop={props.footerTop}
+          footerCountry={props.footerCountry}
+        />
       </div>
     </div>
   );
